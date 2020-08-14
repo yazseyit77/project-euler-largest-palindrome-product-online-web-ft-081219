bb2921823a1b7e877db61af55414b64101beb1ff
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-14 19:19:18 +0000

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+
+def largest_palindrome_product
+  largest = 0
+  (100...999).each do |x|
+    (100...999).each do |y|
+      if(x * y).to_s == (x * y).to_s.reverse && x * y > largest
+        largest = x * y
+      end
+    end
+  end
+  largest
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,17 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+  attr_reader :answer
+
+  def answer
+    largest = 0
+    (100...999).each do |x|
+      (100...999).each do |y|
+        if(x * y).to_s == (x * y).to_s.reverse && x * y > largest
+          largest = x * y
+        end
+      end
+    end
+    largest
+  end
+end
