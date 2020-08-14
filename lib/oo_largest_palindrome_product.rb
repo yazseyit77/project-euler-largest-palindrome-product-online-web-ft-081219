a3e33cd87b5f60b2489bbea2208118cb3c35fd7e
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-14 19:18:58 +0000

    Automatically backed up by Learn

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -4,6 +4,14 @@ class LargestPalindromeProduct
   attr_reader : answer
 
   def answer
-
+    largest = 0
+    (100...999).each do |x|
+      (100...999).each do |y|
+        if(x * y).to_s == (x * y).to_s.reverse && x * y > largest
+          largest = x * y
+        end
+      end
+    end
+    largest
   end
 end
